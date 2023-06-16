8b5b902edf3e9aaf8164ca32c95d502821ba383c
oAoj1 <joaogbmarques@gmail.com>
2023-06-15 22:07:46 -0300

    alterando URL da API

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://localhost:5555'
+    baseURL: 'https://portfolior-back-end-production.up.railway.app'
 })
 
 export default api
