3556454ca6d2acdf9e9ad9fa37d143d6ff5b8805
oAoj1 <joaogbmarques@gmail.com>
2023-06-28 11:42:05 -0300

    adicionando rank D

--- src/components/Section/Skills/index.tsx
+++ src/components/Section/Skills/index.tsx
@@ -22,6 +22,10 @@ export default function Skill(){
         {
             nivel:'c',
             qualificacao:'razoavél'
+        },
+        {
+            nivel:'d',
+            qualificacao:'iniciante'
         }
     ]
 
