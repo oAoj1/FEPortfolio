b1f4f1c2192ac615372e91b55c448808876e4c92
oAoj1 <joaogbmarques@gmail.com>
2024-05-25 17:17:06 -0300

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL: 'http://localhost:5555'
+    baseURL: 'http://localhost:5555' || "https://beportfolio.onrender.com"
 })
 
 export default api
